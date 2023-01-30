b99c110edced4d8655862c19eb9bf7133c9b1086
Carlos Escutia <cescutia@guanajuato.gob.mx>
2023-01-30 14:07:09 -0600

    Se agregan tablas usuarios, roles, opciones_sistema, accesos_sistema y dependencias.

---
+++ application/db/crear_bd.sql
@@ -0,0 +1,50 @@
+DROP TABLE IF EXISTS usuarios;
+CREATE TABLE usuarios (
+    cve_usuario serial, 
+    cve_dependencia integer,
+    cve_rol text,
+    nom_usuario text,
+    usuario text,
+    password text,
+    activo integer
+);
+
+DROP TABLE IF EXISTS roles;
+CREATE TABLE roles (
+    cve_rol text,
+    nom_rol text
+);
+
+DROP TABLE IF EXISTS opciones_sistema;
+CREATE TABLE opciones_sistema (
+    cve_opcion serial,
+    cod_opcion text,
+    nom_opcion text
+);
+
+DROP TABLE IF EXISTS accesos_sistema;
+CREATE TABLE accesos_sistema (
+    cve_acceso serial,
+    cve_rol text,
+    cod_opcion text
+);
+
+DROP TABLE IF EXISTS dependencias;
+CREATE TABLE dependencias (
+    cve_dependencia serial,
+    nom_dependencia text
+);
+
+DROP TABLE IF EXISTS bitacora;
+CREATE TABLE bitacora (
+    cve_evento serial,
+    fecha date,
+    hora time,
+    origen text,
+    usuario text,
+    nom_usuario text,
+    nom_dependencia text,
+    accion text,
+    entidad text,
+    valor text
+);
