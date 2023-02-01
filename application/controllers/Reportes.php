6052bf83957c2eca5563ffe9cba4f706ba52c032
Carlos Escutia <carlosescutia@gmail.com>
2023-02-01 15:59:39 -0600

    Se agrega reporte Listado de bitácora.

--- application/controllers/Reportes.php
+++ application/controllers/Reportes.php
@@ -1,34 +1,121 @@
 <?php
 class Reportes extends CI_Controller {
 
-    public function __construct()
-    {
-        parent::__construct();
-        $this->load->model('usuarios_model');
-        $this->load->model('accesos_sistema_model');
-        $this->load->model('opciones_sistema_model');
-    }
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('usuarios_model');
+		$this->load->model('accesos_sistema_model');
+		$this->load->model('opciones_sistema_model');
+		$this->load->model('bitacora_model');
+	}
 
-    public function index()
-    {
-        if ($this->session->userdata('logueado')) {
-            $cve_rol = $this->session->userdata('cve_rol');
-            $data['cve_usuario'] = $this->session->userdata('cve_usuario');
-            $data['cve_dependencia'] = $this->session->userdata('cve_dependencia');
-            $data['nom_dependencia'] = $this->session->userdata('nom_dependencia');
-            $data['cve_rol'] = $cve_rol;
-            $data['nom_usuario'] = $this->session->userdata('nom_usuario');
-            $data['error'] = $this->session->flashdata('error');
-            $data['accesos_sistema_rol'] = explode(',', $this->accesos_sistema_model->get_accesos_sistema_rol($cve_rol)['accesos']);
-            $data['opciones_sistema'] = $this->opciones_sistema_model->get_opciones_sistema();
+	public function index()
+	{
+		if ($this->session->userdata('logueado')) {
+			$cve_rol = $this->session->userdata('cve_rol');
+			$data['cve_usuario'] = $this->session->userdata('cve_usuario');
+			$data['cve_dependencia'] = $this->session->userdata('cve_dependencia');
+			$data['nom_dependencia'] = $this->session->userdata('nom_dependencia');
+			$data['cve_rol'] = $cve_rol;
+			$data['nom_usuario'] = $this->session->userdata('nom_usuario');
+			$data['error'] = $this->session->flashdata('error');
+			$data['accesos_sistema_rol'] = explode(',', $this->accesos_sistema_model->get_accesos_sistema_rol($cve_rol)['accesos']);
+			$data['opciones_sistema'] = $this->opciones_sistema_model->get_opciones_sistema();
 
-            $this->load->view('templates/header', $data);
-            $this->load->view('reportes/index', $data);
-            $this->load->view('templates/footer');
-        } else {
-            $this->login();
-        }
-    }
+			$this->load->view('templates/header', $data);
+			$this->load->view('reportes/index', $data);
+			$this->load->view('templates/footer');
+		} else {
+			redirect('inicio/login');
+		}
+	}
+
+	public function listado_bitacora_01()
+	{
+		if ($this->session->userdata('logueado')) {
+			$cve_rol = $this->session->userdata('cve_rol');
+			$data['cve_usuario'] = $this->session->userdata('cve_usuario');
+			$data['cve_dependencia'] = $this->session->userdata('cve_dependencia');
+			$data['nom_dependencia'] = $this->session->userdata('nom_dependencia');
+			$data['cve_rol'] = $cve_rol;
+			$data['nom_usuario'] = $this->session->userdata('nom_usuario');
+			$data['error'] = $this->session->flashdata('error');
+			$data['accesos_sistema_rol'] = explode(',', $this->accesos_sistema_model->get_accesos_sistema_rol($cve_rol)['accesos']);
+			$data['opciones_sistema'] = $this->opciones_sistema_model->get_opciones_sistema();
+
+			$filtros = $this->input->post();
+			if ($filtros) {
+				$accion = $filtros['accion'];
+				$entidad = $filtros['entidad'];
+			} else {
+				$accion = '';
+				$entidad = '';
+			}
+
+			$data['accion'] = $accion;
+			$data['entidad'] = $entidad;
+
+            $nom_dependencia = $this->session->userdata['nom_dependencia'];
+			$data['bitacora'] = $this->bitacora_model->get_bitacora($nom_dependencia, $cve_rol, $accion, $entidad);
+
+			$this->load->view('templates/header', $data);
+			$this->load->view('reportes/listado_bitacora_01', $data);
+			$this->load->view('templates/footer');
+		} else {
+			redirect('inicio/iniciar_sesion');
+		}
+	}
+
+	public function listado_bitacora_01_csv()
+	{
+		if ($this->session->userdata('logueado')) {
+			$cve_rol = $this->session->userdata('cve_rol');
+			$data['cve_usuario'] = $this->session->userdata('cve_usuario');
+			$data['cve_dependencia'] = $this->session->userdata('cve_dependencia');
+			$data['nom_dependencia'] = $this->session->userdata('nom_dependencia');
+			$data['cve_rol'] = $cve_rol;
+			$data['nom_usuario'] = $this->session->userdata('nom_usuario');
+			$data['error'] = $this->session->flashdata('error');
+			$data['accesos_sistema_rol'] = explode(',', $this->accesos_sistema_model->get_accesos_sistema_rol($cve_rol)['accesos']);
+			$data['opciones_sistema'] = $this->opciones_sistema_model->get_opciones_sistema();
+
+			$this->load->dbutil();
+			$this->load->helper('file');
+			$this->load->helper('download');
+
+			$filtros = $this->input->post();
+			if ($filtros) {
+				$accion = $filtros['accion'];
+				$entidad = $filtros['entidad'];
+			} else {
+				$accion = '';
+				$entidad = '';
+			}
+
+			if ($cve_rol == 'adm') {
+				$nom_dependencia = '%';
+			}
+
+            $sql = "select b.* from bitacora b where b.nom_dependencia LIKE ? ";
+			$parametros = array();
+			array_push($parametros, "$nom_dependencia");
+			if ($accion <> "") {
+				$sql .= ' and b.accion = ?';
+				array_push($parametros, "$accion");
+			} 
+			if ($entidad <> "") {
+				$sql .= ' and b.entidad = ?';
+				array_push($parametros, "$entidad");
+			} 
+			$sql .= ' order by b.cve_evento desc;';
+			$query = $this->db->query($sql, $parametros);
+
+			$delimiter = ",";
+			$newline = "\r\n";
+			$data = $this->dbutil->csv_from_result($query, $delimiter, $newline);
+			force_download("listado_bitacora_01.csv", $data);
+		}
+	}
 
 }
-
---
+++ application/views/reportes/listado_bitacora_01.php
@@ -0,0 +1,85 @@
+<main role="main" class="ml-sm-auto px-4 mb-3 col-print-12">
+    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <div class="col-sm-12 alternate-color">
+            <form method="post" action="<?= base_url() ?>reportes/listado_bitacora_01">
+                <div class="row">
+                    <div class="col-sm-8 text-start">
+                        <h1 class="h2">Bitácora de actividad</h1>
+                    </div>
+                    <div class="col-sm-4 text-end">
+                        <button formaction="<?= base_url() ?>reportes/listado_bitacora_01_csv" class="btn btn-primary">Exportar a excel</button>
+                        <a href="javascript:window.print()" class="btn btn-primary">Generar pdf</a>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-12 align-self-center">
+                        <div class="row">
+                            <div class="col-2">
+                                <select class="form-select form-select-sm" name="accion">
+                                    <option value="" <?= ($accion == '') ? 'selected' : '' ?>>Todas las acciones</option>
+                                    <option value="login" <?= ($accion == 'login') ? 'selected' : '' ?>>login</option>
+                                    <option value="logout" <?= ($accion == 'logout') ? 'selected' : '' ?>>logout</option>
+                                    <option value="agregó" <?= ($accion == 'agregó') ? 'selected' : '' ?>>agregó</option>
+                                    <option value="modificó" <?= ($accion == 'modificó') ? 'selected' : '' ?>>modificó</option>
+                                    <option value="eliminó" <?= ($accion == 'eliminó') ? 'selected' : '' ?>>eliminó</option>
+                                </select>
+                            </div>
+                            <div class="col-2">
+                                <select class="form-select form-select-sm" name="entidad">
+                                    <option value="" <?= ($entidad == '') ? 'selected' : '' ?>>Todas las entidades</option>
+                                    <option value="usuarios" <?= ($entidad == 'usuarios') ? 'selected' : '' ?>>usuarios</option>
+                                    <option value="opciones_sistema" <?= ($entidad == 'opciones_sistema') ? 'selected' : '' ?>>opciones_sistema</option>
+                                    <option value="accesos_sistema" <?= ($entidad == 'accesos_sistema') ? 'selected' : '' ?>>accesos_sistema</option>
+                                    <option value="dependencias" <?= ($entidad == 'dependencias') ? 'selected' : '' ?>>dependencias</option>
+                                </select>
+                            </div>
+                            <div class="col-1">
+                                <button class="btn btn-success btn-sm">Filtrar</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+
+    <div style="min-height: 46vh">
+        <div class="row">
+            <div class="col-md-12">
+                <table class="table table-striped table-sm">
+                    <thead>
+                        <tr>
+                            <th scope="col">Id</th>
+                            <th scope="col">Fecha</th>
+                            <th scope="col">Hora</th>
+                            <th scope="col">Origen</th>
+                            <th scope="col">Usuario</th>
+                            <th scope="col">Nombre de usuario</th>
+                            <th scope="col">Dependencia</th>
+                            <th scope="col">Acción</th>
+                            <th scope="col">Entidad</th>
+                            <th scope="col">Valor</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($bitacora as $bitacora_item) { ?>
+                        <tr>
+                            <td><?= $bitacora_item['cve_evento'] ?></td>
+                            <td><?= empty($bitacora_item['fecha']) ? '' : date('d/m/y', strtotime($bitacora_item['fecha'])) ?></td>
+                            <td><?= $bitacora_item['hora'] ?></td>
+                            <td><?= $bitacora_item['origen'] ?></td>
+                            <td><?= $bitacora_item['usuario'] ?></td>
+                            <td><?= $bitacora_item['nom_usuario'] ?></td>
+                            <td><?= $bitacora_item['nom_dependencia'] ?></td>
+                            <td><?= $bitacora_item['accion'] ?></td>
+                            <td><?= $bitacora_item['entidad'] ?></td>
+                            <td><?= $bitacora_item['valor'] ?></td>
+                        </tr>
+                        <?php } ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+
+</main>
