90b593a5cb8b226f505d6b8b1df5258bb4982904
Carlos Escutia <carlosescutia@gmail.com>
2023-02-03 16:32:49 -0600

    Se inicia formulario de captura de evaluacion actual.

--- application/views/proyectos/detalle.php
+++ application/views/proyectos/detalle.php
@@ -8,10 +8,10 @@
         </div>
     </div>
     <div class="row">
-        <div class="col-md-6">
+        <div class="col-md-8">
             <?php include 'evaluacion_actual.php' ?>
         </div>
-        <div class="col-md-6">
+        <div class="col-md-4">
             <?php include 'historico_evaluaciones.php' ?>
         </div>
     </div>
