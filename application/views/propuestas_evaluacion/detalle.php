771bb20f5f6a6f694152dd015b18d7f92622ae7d
Carlos Escutia <cescutia@guanajuato.gob.mx>
2023-02-10 09:46:33 -0600

    Se elimina subrayado de enlace en botones.

--- application/views/propuestas_evaluacion/detalle.php
+++ application/views/propuestas_evaluacion/detalle.php
@@ -77,7 +77,7 @@
     <hr />
     <div class="form-group row">
         <div class="col-sm-10">
-            <a href="<?=base_url()?>proyectos/detalle/<?=$propuesta_evaluacion['cve_proyecto']?>" class="btn btn-secondary">Volver</a>
+            <a href="<?=base_url()?>proyectos/detalle/<?=$propuesta_evaluacion['cve_proyecto']?>" class="btn btn-secondary boton">Volver</a>
         </div>
     </div>
 
