7d642ba731d8cc81879f6eac098bf87dd12a23f4
Carlos Escutia <cescutia@guanajuato.gob.mx>
2023-03-03 09:53:34 -0600

    Se modifica base_url para producción.

--- application/config/production/config.php
+++ application/config/production/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://iplaneg.guanajuato.gob.mx/siseval/';
+$config['base_url'] = 'https://iplaneg.guanajuato.gob.mx/siseval/';
 
 /*
 |--------------------------------------------------------------------------
