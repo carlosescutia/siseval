cfb16f7f02a7956850f211f1a15f302844ae0284
Carlos Escutia <cescutia@guanajuato.gob.mx>
2023-11-01 11:37:25 -0600

    Se implementa área pública.

---
+++ application/views/publico/index.php
@@ -0,0 +1,62 @@
+<main role="main" class="ml-sm-auto px-4">
+    <div class="row">
+        <div class="col-sm-12 mt-2">
+            <div class="row">
+                <div class="col-sm-1 fw-bold">
+                    <p>Clave P/Q</p>
+                </div>
+                <div class="col-sm-4 fw-bold">
+                    <p>Evaluación</p>
+                </div>
+                <div class="col-sm-1 fw-bold">
+                    <p>Año</p>
+                </div>
+                <div class="col-sm-1 fw-bold">
+                    <p>Tipo</p>
+                </div>
+                <div class="col-sm-1 fw-bold">
+                    <p>Responsable</p>
+                </div>
+                <div class="col-sm-3 fw-bold">
+                    <p>TdR</p>
+                </div>
+            </div>
+        </div>
+        <?php 
+        $prefijo = 'tr';
+        $icono = "bi-filetype-pdf";
+        $tipo_archivo = 'pdf';
+        $ruta = 'doc/gestion/';
+        foreach ($proyectos as $proyectos_item) { 
+            $nombre_archivo = $prefijo . '_' . strtolower($proyectos_item['cve_proyecto']) . '.' . $tipo_archivo ;
+            $nombre_archivo_fs = './' . $ruta . $nombre_archivo ;
+            $nombre_archivo_url = base_url() . $ruta . $nombre_archivo;
+            if ( file_exists($nombre_archivo_fs) ) { ?>
+                <div class="col-sm-12 alternate-color">
+                    <div class="row">
+                        <div class="col-sm-1">
+                            <p><?= $proyectos_item['cve_proyecto'] ?></p>
+                        </div>
+                        <div class="col-sm-4">
+                            <p> <?= $proyectos_item['nom_proyecto'] ?> </p>
+                        </div>
+                        <div class="col-sm-1">
+                            <p><?= $proyectos_item['periodo'] ?></p>
+                        </div>
+                        <div class="col-sm-1">
+                            <p><?= $proyectos_item['nom_tipo_evaluacion'] ?></p>
+                        </div>
+                        <div class="col-sm-1">
+                            <p><?= $proyectos_item['nom_dependencia'] ?></p>
+                        </div>
+                        <div class="col-sm-3">
+                            <p><a href="<?=$nombre_archivo_url?>" target="_blank"><i class="bi <?=$icono?> documento-g"></i></a></p>
+                        </div>
+                    </div>
+                </div>
+            <?php } ?>
+        <?php } ?>
+    </div>
+    <hr />
+
+</main>
---
+++ application/views/templates/pubheader.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="icon" href="<?=base_url()?>img/favicon.png" sizes="16x16" type="image/png" />
+
+        <title>Sistema de Evaluación de Guanajuato</title>
+
+        <!-- global css -->
+        <link rel="stylesheet" type="text/css" href="<?=base_url()?>css/publico.css" />
+        <!-- app css -->
+        <link rel="stylesheet" type="text/css" href="<?=base_url()?>css/siseval.css" />
+
+        <!-- bootstrap 5.3 -->
+        <link href="<?=base_url()?>css/bootstrap.min.css" rel="stylesheet"/>
+        <link href="<?=base_url()?>css/bootstrap-icons.css" rel="stylesheet"/>
+        <script src="<?=base_url()?>js/bootstrap.bundle.min.js"></script>
+
+        <!-- jquery -->
+        <script src="https://code.jquery.com/jquery-3.6.3.min.js" integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
+
+        <!-- bootstrap 5.3 
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+        -->
+
+    </head>
+    <body>
