a644193b726d12457797b0244bf5a40cc4ca276f
Carlos Escutia <carlosescutia@gmail.com>
2024-01-25 15:51:44 -0600

    Se parametriza conexión a base de datos, url base mediante archivo .env

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '';
+$config['base_url'] = getenv('BASE_URL');
 
 /*
 |--------------------------------------------------------------------------
@@ -225,7 +225,7 @@ $config['allow_get_array'] = TRUE;
 | your log files will fill up very fast.
 |
 */
-$config['log_threshold'] = 0;
+$config['log_threshold'] = getenv('LOG_THRESHOLD');
 
 /*
 |--------------------------------------------------------------------------
